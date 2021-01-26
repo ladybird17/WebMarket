1932ef734117778a2fac73f4ed9c71a8e57217ed
ladybird17 <modalon@naver.com>
2021-01-26 16:45:05 +0900

    p278 하다가 안된채로 일단 올림

--- WebContent/resource/js/validation.js
+++ WebContent/resource/js/validation.js
@@ -1,7 +1,4 @@
-/**
- * 
- */
- 
+
  function CheckAddProduct(){
  	var productId = document.getElementById("productId");
  	var name = document.getElementById("name");
@@ -9,9 +6,7 @@
  	var unitsInstock = document.getElementById("unitsInstock");
  	
  	//상품 아이디 체크
- 	if(!check(/^P[0-9]{4,11}$/, productId,
- 	"[상품코드]\nP와 숫자를 조합하여 5~12자까지 입력하세요.
- 	\n첫 글자는 반드시 P로 시작하세요."))
+ 	if(!check(/^P[0-9]{4,11}$/, productId,"[상품코드]\nP와 숫자를 조합하여 5~12자까지 입력하세요.\n첫 글자는 반드시 P로 시작하세요."))
  		return false;
  		
  	//상품명 체크
